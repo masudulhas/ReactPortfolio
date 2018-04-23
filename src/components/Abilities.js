9f66cffcd2d47425204fdeda3843f952984de146
masudulhas <masudul.has@gmail.com>
2018-04-23 23:27:12 +0200

    modified

--- src/components/Abilities.js
+++ src/components/Abilities.js
@@ -5,46 +5,46 @@ class Abilities extends Component {
         return (
             <div className="container">
               <h2 className="tagline">Abilities</h2>
-      <p className="lead">&ldquo;Life without knowledge is death in disguise.&rdquo;<br/>- Talib Kweli</p>
-      <hr/>
-      <aside className="left line">
-        <h3>Skills</h3>
-        <h4>Backend programming:</h4>
-        <p> PHP, Node.js, Java, JavaEE, C#</p>
-      </aside>
-      <aside className="right line">
-        <br/>
-        <h4>Web technologies:</h4>
-        <p>HTML5, CSS3, Bootstrap, jQuery, JavaScript, MVC5, React, Vue, Vuetify, Redux, JSON</p>
-      </aside>
+              <p className="lead">&ldquo;Life without knowledge is death in disguise.&rdquo;<br/>- Talib Kweli</p>
+              <hr/>
+              <aside className="left line">
+                <h3>Skills</h3>
+                <h4>Backend programming:</h4>
+                <p> PHP, Node.js, Java, JavaEE, C#</p>
+              </aside>
+              <aside className="right line">
+                <br/>
+                <h4>Web technologies:</h4>
+                <p>HTML5, CSS3, Bootstrap, jQuery, JavaScript, MVC5, React, Vue, Vuetify, Redux, JSON</p>
+              </aside>
 
-      <aside className="left line">
-        <h4>Database:</h4>
-        <p>MySQL, MSSQL</p>
-      </aside>
-      <aside className="right line">
-        <h4>Framework:</h4>
-        <p>Spring MVC, .Net Framework, Entity Framework</p>
-      </aside>
+              <aside className="left line">
+                <h4>Database:</h4>
+                <p>MySQL, MSSQL</p>
+              </aside>
+              <aside className="right line">
+                <h4>Framework:</h4>
+                <p>Spring MVC, .Net Framework, Entity Framework</p>
+              </aside>
 
-      <aside className="left line">
-        <h4>IDE(s):</h4>
-        <p> NetBeans, Eclipse, Visual Studio</p>
-      </aside>
-      <aside className="right line">
-        <h4>OS:</h4>
-        <p>Windows, Linux</p>
-      </aside>
+              <aside className="left line">
+                <h4>IDE(s):</h4>
+                <p> NetBeans, Eclipse, Visual Studio</p>
+              </aside>
+              <aside className="right line">
+                <h4>OS:</h4>
+                <p>Windows, Linux</p>
+              </aside>
 
-      <aside className="left line">
-        <h3 >Languages</h3>
-      </aside>
-      <aside className="right line">
-          <p>Bengali (Mother language)</p>
-          <p>English</p>
-          <p>Swedish</p>
-      </aside>
-      </div>
+              <aside className="left line">
+                <h3 >Languages</h3>
+              </aside>
+              <aside className="right line">
+                  <p>Bengali (Mother language)</p>
+                  <p>English</p>
+                  <p>Swedish</p>
+              </aside>
+            </div>
         );
     }
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Contact extends Component {
     render() {
         return (
             <div className="container">
               <h2 className="tagline">Contact</h2>
-      <p class="lead">&ldquo;If I had asked people what they wanted, they would have said faster horses. &rdquo;<br />- Henry Ford</p>
-      <hr/>
-      <aside className="left">
-        <h4>Follow me</h4>
-        <a href="https://se.linkedin.com/in/masudulhasan" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i>Linkedin</a><br/>
-        <a href="https://twitter.com/masud33bd" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</a>
-      </aside>
-      <aside class="right">
-        <h4>E-mail: masudulhas@gmail.com</h4>
-        <h4>phone: 0046769486680</h4>
-      </aside>
-
-<aside className="foot">
-<h5>  &copy; 2018, All rights reserve</h5>
-</aside>
+                <p class="lead">&ldquo;If I had asked people what they wanted, they would have said faster horses. &rdquo;<br />- Henry Ford</p>
+                <hr/>
+                <div>
+                <Form>
+                  <FormGroup row>
+                    <Label for="exampleName" sm={2} size="lg">Name</Label>
+                    <Col sm={10}>
+                      <Input type="name" name="name" id="exampleName" className="formcontrol" placeholder="Name" bsSize="lg" />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleEmail" sm={2}>Email</Label>
+                    <Col sm={10}>
+                      <Input type="email" name="email" id="exampleEmail" className="formcontrol" placeholder="Email" />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="exampleText">Text Area</Label>
+                    <Input type="textarea" name="text" id="exampleText" className="formcontrol" />
+                  </FormGroup>
+                </Form>
+                </div>
+                <aside className="left">
+                  <h4>Follow me</h4>
+                  <a href="https://se.linkedin.com/in/masudulhasan" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i>Linkedin</a><br/>
+                  <a href="https://twitter.com/masud33bd" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</a>
+                </aside>
+                <aside className="right">
+                  <h4>E-mail: masudulhas@gmail.com</h4>
+                  <h4>phone: 0046769486680</h4>
+                </aside>
+                <aside className="foot">
+                <h5>  &copy; 2018, All rights reserve</h5>
+                </aside>
             </div>
         );
     }
